ae8b36c4f79e3efa53b10213aa8be4fba62aa6d0
Shynkarenko Volodymyr <shynkarenko@gmail.com>
2019-05-05 09:17:17 +0300

    Initial Commit

---
+++ mvc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mvc
+//
+//  Created by Vladimir on 5/5/19.
+//  Copyright © 2019 Volodymyr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
